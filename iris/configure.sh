e39b040c68c8c5d431712bf4a14bc9a6f7b82177
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2024-06-13 08:13:00 +0200

    init

--- iris/configure.sh
+++ iris/configure.sh
@@ -2,8 +2,10 @@ cat << EOF | iris session iris
 zn "%sys"
 do ##class(Security.Users).UnExpireUserPasswords("*")
 do ##class(%SYSTEM.CSP).SetConfig("CSPConfigName","$HOSTNAME")
-# zn "user"
-# do ##class(%SYSTEM.OBJ).Load("/iris/ipm.cls","ck")
-# do ##class(iris.ipm).install()
+zn "user"
+set ^trace(\$i(^trace))=\$zdt(\$h,3)
+zw ^trace
+// ##class(%SYSTEM.OBJ).Load("/iris/ipm.cls","ck")
+// ##class(iris.ipm).install()
 halt
 EOF

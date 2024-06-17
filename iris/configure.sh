753712ea945c5edf2bdb8482325d0ecd2844de9d
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2024-06-17 13:43:20 +0200

    init

--- iris/configure.sh
+++ iris/configure.sh
@@ -3,6 +3,7 @@ zn "%sys"
 do ##class(Security.Users).UnExpireUserPasswords("*")
 do ##class(%SYSTEM.CSP).SetConfig("CSPConfigName","$HOSTNAME")
 zn "user"
+do ##class(%EnsembleMgr).EnableNamespace("USER")
 set ^trace(\$i(^trace))=\$zdt(\$h,3)
 zw ^trace
 // ##class(%SYSTEM.OBJ).Load("/iris/ipm.cls","ck")
